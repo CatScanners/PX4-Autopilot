b1194732deec532fe8fb4da3119b6593a24db4d9
Nestor <nestor94gonzalez@gmail.com>
2025-11-28 01:41:44 +0000

    Fix: Replace shell aliases with functions for better portability

--- platforms/posix/cmake/px4_impl_os.cmake
+++ platforms/posix/cmake/px4_impl_os.cmake
@@ -144,7 +144,7 @@ function(px4_posix_generate_alias)
 		endforeach()
 		if (MAIN)
 			set(alias_string
-				"${alias_string}alias ${MAIN}='${PREFIX}${MAIN} --instance $px4_instance'\n"
+				"${alias_string}${MAIN}() { ${PREFIX}${MAIN} --instance $px4_instance \"$@\"; }\n"
 			)
 		endif()
 	endforeach()
